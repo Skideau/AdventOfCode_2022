1d0f92d684310ec32f754b7f418c808a9e5bdb7b
Skido <barbarossaSkido@gmail.com>
2022-12-10 12:58:38 +0100

    Updates solution for first part of day9's challenge

--- day9/solver.go
+++ day9/solver.go
@@ -1,54 +1,133 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math"
+	"os"
+	"strconv"
+	"strings"
 )
 
 type ropeEnd struct {
+	name string
 	posX int
 	posY int
 }
 
-func (ropePart *ropeEnd) Move(direction rune, repetition int) {
+func (ropePart *ropeEnd) MoveDiag(distVect ropeEnd) {
+	if !math.Signbit(float64(distVect.posX)) {
+		ropePart.posX++
+	} else {
+		ropePart.posX--
+	}
+
+	if !math.Signbit(float64(distVect.posY)) {
+		ropePart.posY++
+	} else {
+		ropePart.posY--
+	}
+	// fmt.Printf("Diag move: %v\n", ropePart)
+}
+
+func (ropePart *ropeEnd) MoveStraight(direction string) {
 	switch direction {
-	case 'U': // UP
-	case 'R': // RIGHT
-	case 'D': // DOWN
-	case 'L': // LEFT
+	case "U": // UP
+		ropePart.posY++
+	case "R": // RIGHT
+		ropePart.posX++
+	case "D": // DOWN
+		ropePart.posY--
+	case "L": // LEFT
+		ropePart.posX--
+	}
+	// fmt.Printf("Straight move: %v\n", ropePart)
+}
+
+func (tail *ropeEnd) Follow(head *ropeEnd) (moved bool) {
+	moved = false
+	if maxDist(head, tail) > 1 {
+		moveVect := ropeVect(head, tail)
+		if head.posX == tail.posX {
+			// Move Straight vertically
+			if !math.Signbit(float64(moveVect.posY)) {
+				tail.MoveStraight("U")
+			} else {
+				tail.MoveStraight("D")
+			}
+		} else if head.posY == tail.posY {
+			// MoveStraight horizontally
+			if !math.Signbit(float64(moveVect.posX)) {
+				tail.MoveStraight("R")
+			} else {
+				tail.MoveStraight("L")
+			}
+		} else {
+			// Move diagonaly
+			tail.MoveDiag(moveVect)
+		}
+		moved = true
+		return
+	}
+	return
+}
+
+func updatePositionHistoric(posHistory *[]ropeEnd, newPos *ropeEnd) {
+	valFound := false
+	for _, historicVal := range *posHistory {
+		valFound = newPos.posX == historicVal.posX && newPos.posY == historicVal.posY
+		if valFound {
+			break
+		}
+	}
+
+	if !valFound {
+		*posHistory = append(*posHistory, *newPos)
+		// fmt.Printf("History updated with value: %v\n", *newPos)
 	}
 }
 
-func manhattanDist(head *ropeEnd, tail *ropeEnd) int {
-	return int(math.Abs(float64((*head).posX-(*tail).posX))) + int(math.Abs(float64((*head).posY-(*tail).posY)))
-}
+// func manhattanDist(head *ropeEnd, tail *ropeEnd) int {
+// 	return int(math.Abs(float64((*head).posX-(*tail).posX))) + int(math.Abs(float64((*head).posY-(*tail).posY)))
+// }
 
 func maxDist(head *ropeEnd, tail *ropeEnd) int {
 	return int(math.Max(math.Abs(float64((*head).posX-(*tail).posX)), math.Abs(float64((*head).posY-(*tail).posY))))
 }
 
 func ropeVect(head *ropeEnd, tail *ropeEnd) ropeEnd {
-	return ropeEnd{head.posX - tail.posX, head.posY - tail.posY}
+	return ropeEnd{"Vect", head.posX - tail.posX, head.posY - tail.posY}
 }
 
 func main() {
-	// file, err := os.Open("input.txt")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer file.Close()
+	file, err := os.Open("../inputs/day9/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-	// scanner := bufio.NewScanner(file)
-	// for scanner.Scan() {
-	// 	currentLine := scanner.Text()
-	// }
-	head := ropeEnd{0, 0}
-	tail := ropeEnd{0, 0}
-	for y := -2; y <= 2; y++ {
-		head.posY = y
-		for x := -2; x <= 2; x++ {
-			head.posX = x
-			fmt.Printf("Head%v, maxDist=%d, manhDist=%d, vect=%v\n", head, maxDist(&head, &tail), manhattanDist(&head, &tail), ropeVect(&head, &tail))
+	head := ropeEnd{"Head", 0, 0}
+	tail := ropeEnd{"Tail", 0, 0}
+	tailHistory := append([]ropeEnd{}, tail)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		currentLine := scanner.Text()
+		moveInstructions := strings.Fields(currentLine)
+		headDirection := moveInstructions[0]
+		headMoveRepetition, _ := strconv.Atoi(moveInstructions[1])
+		// fmt.Printf("Head moving: %d times direction: %v\n", headMoveRepetition, headDirection)
+		for moveCounter := 0; moveCounter < headMoveRepetition; moveCounter++ {
+			head.MoveStraight(headDirection)
+			if tail.Follow(&head) {
+				updatePositionHistoric(&tailHistory, &tail)
+			}
 		}
 	}
+
+	println("=====PART1=====")
+	fmt.Printf("Tail tooked %d different positions during heads journey\n", len(tailHistory))
+
+	println("\n=====PART2=====")
 }
